eabcf8120ff3534037519e2d35bae61db1ba0cd9
haneefnb <fromhaneef@gmail.com>
2022-06-29 11:07:10 +0530

    OK1 added

--- src/main/java/com/haneefs/springbootpoc/Application.java
+++ src/main/java/com/haneefs/springbootpoc/Application.java
@@ -8,5 +8,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("------OK---------------");
+        System.out.println("------OK1--------------");
     }
 }
